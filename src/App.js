d1b6ee6561bdd7e8a466205312e8d90d62163fa1
Daniel Page <73043391+Daniel-Page@users.noreply.github.com>
2022-05-09 09:46:59 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import FaultEquationCalculator from "./fault_equation_calculator";
-import { Routes, Route, HashRouter } from "react-router-dom";
 import React, { Suspense } from "react";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route
-              path="./"
-              element={<FaultEquationCalculator />}
-            />
-          </Routes>
-        </Suspense>
-      </HashRouter>
+      <FaultEquationCalculator />
     </div>
   );
 }
